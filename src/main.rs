c360de1e890761dbd5a8ee7735b9f0d8835e5a1a
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-06 16:34:25 +0900

    Use stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self, Read};
+
 fn main() {
+    for b in io::stdin().bytes() {
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                println!("{}", c);
+            }
+            Err(e) => {
+                panic!(e);
+            }
+        }
+    }
     println!("Hello, world!");
 }
