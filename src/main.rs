04c1738583d4ff8b9afce74e44461f88b7cbf7fc
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-07 18:48:23 +0900

    Fix to quit on Ctrl + Q

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use rust_editor::{error::Error, input_unix::StdinRaw};
 
+fn to_ctrl_byte(c: char) -> u8 {
+    (c as u8) & 0b0001_1111
+}
+
 fn main() -> Result<(), Error> {
     let input = StdinRaw::new()?;
     'app: loop {
@@ -7,7 +11,7 @@ fn main() -> Result<(), Error> {
             let b = b?;
             let c = b as char;
             print!("{} ({:?})\r\n", c, b);
-            if c == 'q' {
+            if b == to_ctrl_byte('q') {
                 print!("quit\r\n");
                 break 'app;
             }
