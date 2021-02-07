aff604a448ccf8e845afd7d843bcc805c7edb177
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-07 16:31:05 +0900

    Enable raw mode with termios

---
+++ src/input_unix.rs
@@ -0,0 +1,45 @@
+use super::error::Error;
+use std::{
+    io::{self, Read},
+    os::unix::io::AsRawFd,
+};
+use termios::*;
+
+#[cfg(unix)]
+pub struct StdinRaw {
+    fd: i32,
+    orig: Termios,
+}
+
+impl StdinRaw {
+    pub fn new() -> Result<Self, Error> {
+        let fd = io::stdin().as_raw_fd();
+        let mut termios = Termios::from_fd(fd)?;
+        let orig = termios;
+
+        // Set terminal raw mode. Disable echo back, canonical mode, signals (SIGINT, SIGTSTP) and Ctrl+V.
+        termios.c_lflag &= !(ECHO | ICANON | ISIG | IEXTEN);
+        // Disable control flow mode (Ctrl+Q/Ctrl+S) and CR-to-NL translation
+        termios.c_iflag &= !(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+        // Disable output processing such as \n to \r\n translation
+        termios.c_oflag &= !OPOST;
+        // Ensure character size is 8bits
+        termios.c_cflag |= CS8;
+        // Do not wait for next byte with blocking since reading 0 byte is permitted
+        termios.c_cc[VMIN] = 0;
+        // Set read timeout to 1/10 second it enables 100ms timeout on read()
+        termios.c_cc[VTIME] = 1;
+        // Apply terminal configurations
+        tcsetattr(fd, TCSAFLUSH, &termios)?;
+
+        Ok(StdinRaw { fd, orig })
+    }
+
+    pub fn dispose(self) {
+        tcsetattr(self.fd, TCSAFLUSH, &self.orig).unwrap();
+    }
+
+    pub fn bytes(&self) -> Vec<Result<u8, std::io::Error>> {
+        io::stdin().bytes().collect()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use rust_editor::error::Error;
-use std::io::{self, Read};
+use rust_editor::{error::Error, input_unix::StdinRaw};
 
 fn main() -> Result<(), Error> {
-    for b in io::stdin().bytes() {
-        let b = b?;
-        let c = b as char;
-        println!("{}", c);
+    let input = StdinRaw::new()?;
+    'app: loop {
+        for b in input.bytes() {
+            let b = b?;
+            let c = b as char;
+            print!("{} ({:?})\r\n", c, b);
+            if c == 'q' {
+                print!("quit\r\n");
+                break 'app;
+            }
+        }
     }
+    input.dispose();
     Ok(())
 }
