426feab45b1706f1917bf74328998642b4cb4aa0
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-08 01:58:12 +0900

    Fix to erase each lines when redraw

--- src/editor.rs
+++ src/editor.rs
@@ -31,7 +31,8 @@ impl Editor {
             let quit = self.process_key_press()?;
             if quit == true {
                 let mut buf = String::new();
-                self.clear_screen(&mut buf);
+                buf.push_str("\x1b[2J");
+                buf.push_str("\x1b[H");
                 print!("{}", buf);
                 break;
             }
@@ -42,7 +43,7 @@ impl Editor {
     fn refresh_screen(&self) -> Result<(), Error> {
         let mut buf = String::new();
         buf.push_str("\x1b[?25l");
-        self.clear_screen(&mut buf);
+        buf.push_str("\x1b[H");
 
         self.draw_rows(&mut buf);
 
@@ -64,16 +65,10 @@ impl Editor {
         Ok(false)
     }
 
-    fn clear_screen(&self, buf: &mut String) {
-        // Clear screen
-        buf.push_str("\x1b[2J");
-        // Move cursor to left top
-        buf.push_str("\x1b[H");
-    }
-
     fn draw_rows(&self, buf: &mut String) {
         for y in 0..self.screen_rows {
             buf.push_str("~");
+            buf.push_str("\x1b[K");
             if y < self.screen_rows - 1 {
                 buf.push_str("\r\n");
             }
