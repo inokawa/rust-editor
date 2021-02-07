c76a56d62e147b5172e7d5337d492d4a80de8c89
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-07 21:54:51 +0900

    Refactor
    a

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ fn to_ctrl_byte(c: char) -> u8 {
     (c as u8) & 0b0001_1111
 }
 
+fn process_key_press(input: &StdinRaw) -> Result<bool, Error> {
+    for b in input.bytes() {
+        let b = b?;
+        let c = b as char;
+        print!("{} ({:?})\r\n", c, b);
+        if b == to_ctrl_byte('q') {
+            return Ok(true);
+        }
+    }
+    Ok(false)
+}
+
 fn main() -> Result<(), Error> {
     let input = StdinRaw::new()?;
-    'app: loop {
-        for b in input.bytes() {
-            let b = b?;
-            let c = b as char;
-            print!("{} ({:?})\r\n", c, b);
-            if b == to_ctrl_byte('q') {
-                print!("quit\r\n");
-                break 'app;
-            }
+    loop {
+        let quit = process_key_press(&input)?;
+        if quit == true {
+            print!("quit\r\n");
+            break;
         }
     }
     Ok(())
