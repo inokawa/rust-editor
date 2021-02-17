926b801e7e4133598a8fca601be4aeba93de3da7
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-17 22:56:48 +0900

    Update web editor

--- web/js/index.ts
+++ web/js/index.ts
@@ -1,4 +1,15 @@
 (async () => {
   const wasm = await import("../pkg/index.js");
   console.log(wasm);
+
+  document
+    .querySelector(".textarea")
+    ?.addEventListener("keydown", (ev: Event) => {
+      let e = ev as KeyboardEvent;
+      console.log(e);
+      console.log(e.code);
+      console.log(e.key);
+      console.log(e.shiftKey);
+      console.log(e.ctrlKey);
+    });
 })();
