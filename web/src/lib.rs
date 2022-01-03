f9fa527de26a5988e64e17fe86ed19f2cf9f76d0
inokawa <48897392+inokawa@users.noreply.github.com>
2022-01-03 23:56:30 +0900

    Rewrite setTimeout with wasm_bindgen_futures

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -4,33 +4,34 @@ pub mod output;
 pub mod xterm;
 
 use core::{Editor, Filer, Input, Output};
-use js_sys::global;
-use std::{cell::RefCell, rc::Rc};
-use wasm_bindgen::{prelude::*, JsCast};
-use web_sys::WorkerGlobalScope;
+use js_sys::{Error, Promise};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen(
+    inline_js = "export function timeout() { return new Promise((resolve)=> setTimeout(resolve)); }"
+)]
+extern "C" {
+    fn timeout() -> Promise;
+}
+
 #[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
+pub async fn main_js() -> Result<(), JsValue> {
     let input = input::WebInput::new().unwrap();
     let mut editor = Editor::new(input, output::WebOutput::new(), filer::WebFile::new()).unwrap();
 
-    let f = Rc::new(RefCell::new(None));
-    let g = f.clone();
-
-    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        editor.run();
-        set_micro_task(f.borrow().as_ref().unwrap());
-    }) as Box<dyn FnMut()>));
-    set_micro_task(g.borrow().as_ref().unwrap());
-
+    loop {
+        JsFuture::from(timeout()).await?;
+        let quit = editor
+            .run()
+            .map_err(|err| JsValue::from(Error::new(&format!("{:?}", err))))?;
+        if quit {
+            break;
+        }
+    }
     Ok(())
-}
-
-fn set_micro_task(f: &Closure<dyn FnMut()>) {
-    let global = global().unchecked_into::<WorkerGlobalScope>();
-    global.set_timeout_with_callback_and_timeout_and_arguments_0(f.as_ref().unchecked_ref(), 0);
 }
